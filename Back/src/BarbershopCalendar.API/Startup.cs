ddc9a365adaca1c0d2822c57d184d9f58d8251dd
fhlottideveloper <fhlottideveloper@gmail.com>
2023-01-03 15:33:28 -0300

    Porjets Configured

--- Back/src/BarbershopCalendar.API/Startup.cs
+++ Back/src/BarbershopCalendar.API/Startup.cs
@@ -1,7 +1,9 @@
+using BarbershopCalendar.Persistence.DataContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,12 @@ namespace BarberShopCalendar.API
         {
 
             services.AddControllers();
+            services.AddDbContext<BarbershopContext>(options => 
+                options.UseNpgsql(Configuration.GetConnectionString("Default")));
+
+            services.AddCors(); 
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BarberShopCalendar.API", Version = "v1" });
@@ -49,6 +57,10 @@ namespace BarberShopCalendar.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(acess => acess.AllowAnyHeader()
+                                  .AllowAnyMethod()
+                                  .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
---
+++ Back/src/BarbershopCalendar.Persistence/DataContext/BarbershopContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarbershopCalendar.Persistence.DataContext
+{
+    public class BarbershopContext : DbContext
+    {
+
+        public BarbershopContext(DbContextOptions<BarbershopContext> options) : base(options)
+        {
+
+        }
+    }
+}
